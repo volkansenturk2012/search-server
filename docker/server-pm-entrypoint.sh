b170fae5ba9e03d056a171a7f4fcee86c220a785
Marc Morera <yuhu@mmoreram.com>
2019-03-04 19:23:22 +0100

    Using new PPM Bridge

--- docker/server-pm-entrypoint.sh
+++ docker/server-pm-entrypoint.sh
@@ -3,4 +3,4 @@
 rm -Rf /var/www/apisearch/var/cache
 php /var/www/apisearch/bin/console cache:warmup --env=prod --no-debug --no-interaction
 php /var/www/apisearch/bin/console apisearch-server:server-configuration --env=prod --no-debug --no-interaction
-php /var/www/apisearch/vendor/bin/ppm start --host=0.0.0.0 --port=8200 --workers=3 --bootstrap=OneBundleApp\\PPM\\Adapter --app-env=prod --debug=0 --logging=0
+php /var/www/apisearch/vendor/bin/ppm start --host=0.0.0.0 --port=8200 --workers=3 --bootstrap=OneBundleApp\\PPM\\Adapter --bridge=OneBundleApp\\PPM\\Bridge --app-env=prod --debug=0 --logging=0
