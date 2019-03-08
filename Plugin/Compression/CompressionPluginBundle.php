d09dc780eb91dccf484d6d51d2ec4dce73639b0b
Marc Morera <yuhu@mmoreram.com>
2019-03-08 13:10:30 +0100

    Added compression plugin
    - GZIP
    - Deflate

---
+++ Plugin/Compression/CompressionPluginBundle.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the Apisearch Server
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ */
+
+declare(strict_types=1);
+
+namespace Apisearch\Plugin\Compression;
+
+use Apisearch\Server\ApisearchServerBundle;
+use Apisearch\Server\Domain\Plugin\Plugin;
+use Mmoreram\BaseBundle\BaseBundle;
+use Mmoreram\BaseBundle\SimpleBaseBundle;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Class CompressionPluginBundle
+ */
+class CompressionPluginBundle extends SimpleBaseBundle implements Plugin
+{
+    /**
+     * Return all bundle dependencies.
+     *
+     * Values can be a simple bundle namespace or its instance
+     *
+     * @param KernelInterface $kernel
+     *
+     * @return array
+     */
+    public static function getBundleDependencies(KernelInterface $kernel): array
+    {
+        return [
+            ApisearchServerBundle::class,
+            BaseBundle::class
+        ];
+    }
+
+    /**
+     * get config files.
+     *
+     * @return array
+     */
+    public function getConfigFiles(): array
+    {
+        return [
+            'listeners',
+        ];
+    }
+
+    /**
+     * Get plugin name.
+     *
+     * @return string
+     */
+    public function getPluginName(): string
+    {
+        return 'compression';
+    }
+}
---
+++ Plugin/Compression/Tests/Functional/BasicCompressionTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the Apisearch Server
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ */
+
+declare(strict_types=1);
+
+namespace Apisearch\Plugin\Compression\Tests\Functional;
+use Apisearch\Query\Query;
+use Apisearch\Server\Tests\Functional\CurlFunctionalTest;
+
+/**
+ * Class BasicCompressionTest
+ */
+class BasicCompressionTest extends CurlFunctionalTest
+{
+    /**
+     * Test gzip compression
+     */
+    public function testGzipCompression()
+    {
+        $this->query(Query::createMatchAll());
+        $length1 = self::$lastResponse['length'];
+        $this->query(Query::createMatchAll());
+        $length2 = self::$lastResponse['length'];
+        $this->assertEquals($length1, $length2);
+        $result = $this->query(
+            Query::createMatchAll(),
+            null, null, null, [], [
+                'Accept-Encoding: gzip'
+            ]
+        );
+        $this->assertCount(5, $result->getItems());
+        $lengthReduced = self::$lastResponse['length'];
+        $this->assertTrue($length1 > $lengthReduced);
+    }
+
+    /**
+     * Test deflate compression
+     */
+    public function testDeflateCompression()
+    {
+        $this->query(Query::createMatchAll());
+        $length1 = self::$lastResponse['length'];
+        $result = $this->query(
+            Query::createMatchAll(),
+            null, null, null, [], [
+                'Accept-Encoding: deflate'
+            ]
+        );
+        $this->assertCount(5, $result->getItems());
+        $lengthReduced = self::$lastResponse['length'];
+        $this->assertTrue($length1 > $lengthReduced);
+    }
+}
